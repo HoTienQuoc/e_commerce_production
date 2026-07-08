0c5488f570022a495240007c743acedb14a946f1
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-09 00:25:32 +0700

    async tasks: introduction to celery

--- backend/backend_admin/backend_admin/settings.py
+++ backend/backend_admin/backend_admin/settings.py
@@ -157,3 +157,13 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ]
 }
+
+CELERY_TIMEZONE = "Africa/Nairobi"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30*60 # 30 minutes
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKEN_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
