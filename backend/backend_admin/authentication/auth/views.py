bf6d19e2c27afca03ffeebed57761acc0db17d69
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-09 03:14:48 +0700

    user registration: the api view part2 & finalization

--- backend/backend_admin/authentication/auth/views.py
+++ backend/backend_admin/authentication/auth/views.py
@@ -1,4 +1,8 @@
 import logging
+import re
+import stat
+import time
+from token import tok_name
 import traceback
 from django.utils import timezone
 from django.conf import settings
@@ -27,5 +31,40 @@ class UserRegistrationView(BaseAPIView):
             phone_number = request.data.get('phone_number')
             first_name = request.data.get('first_name')
             last_name = request.data.get('last_name')
+
+            success, response_data, status_code = AuthenticationService.register(
+                email = email,
+                password= password,
+                phone_number=phone_number,
+                first_name=first_name,
+                last_name=last_name,
+                request=request.META,
+            )
+
+            # create response object
+            response = Response(standardized_response(**response_data), status = status_code)
+
+            if success and status_code in (200, 201) and settings.JWT_COOKIE_SECURE:
+                tokens = response_data.get('data', {}).get('tokens', {})
+                if 'refresh_token' in tokens and 'refresh_expires_in' in tokens:
+                    response.set_cookie(
+                        key = settings.JWT_COOKIE_NAME,
+                        value=tokens['refresh_token'],
+                        expires= timezone.now() + timedelta(seconds=['refresh_expires_in']), # pyright: ignore[reportArgumentType]
+                        secure=True,
+                        httponly=True,
+                        samesite='Strict',
+                        path='/',
+                        domain=settings.SESSION_COOKIE_DOMAIN
+                    )
+
+                # Set CSRF token
+                if success:
+                    get_token(request)
+                return response
+        except Exception as e:
+            logger.error(f"Registration error: {str(e)}")
+            logger.error(traceback.format_exc())
+            return Response(standardized_response(success=False, error="Registration failed. Please try again."),status=status.HTTP_400_BAD_REQUEST)
 
             
