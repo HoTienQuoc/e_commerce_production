1c34acaca42c1f3984e12107e81b34311ab62a9d
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-09 01:55:17 +0700

    async tasks: creating the "send email" task

--- backend/backend_admin/authentication/verification/tasks.py
+++ backend/backend_admin/authentication/verification/tasks.py
@@ -2,3 +2,30 @@ import logging
 from celery import shared_task
 from django.contrib.auth import get_user_model
 from emails import EmailService
+
+User = get_user_model()
+logger = logging.getLogger(__name__)
+
+@shared_task(
+    bind = True, 
+    autoretry_for = (Exception,), 
+    retry_kwargs = {'max_retries':3}, 
+    retry_backoff = True, 
+    retry_back_off_max = 60, 
+    name = "authentication.verification.send_verification_email"
+)
+
+def send_verification_email_task(self, user_id:int):
+    """celery task to send a verification email."""
+    try:
+        user = User.objects.get(id = user_id)
+        if not user.is_verified: # pyright: ignore[reportAttributeAccessIssue]
+            logger.info(f"Executing verification email task for user: {user.email}") # pyright: ignore[reportAttributeAccessIssue]
+            EmailService.send_verification_email(user)
+        else:
+            logger.info(f"Skipping verification email for already verified user: {user.email}") # pyright: ignore[reportAttributeAccessIssue]
+    except User.DoesNotExist:
+        logger.warning(f"User with ID {user_id} not found. Cannot send verification email.")
+    except Exception as e:
+        logger.error(f"Failed to send verification email for user {user_id} on attempt {self.retries}: {e}")
+        raise e
