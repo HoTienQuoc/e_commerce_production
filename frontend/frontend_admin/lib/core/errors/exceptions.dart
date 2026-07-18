28cead003f5ed58d71499354fce5c348d11e2cdb
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-18 20:39:10 +0700

    Data Layer: Abstract Api Client & Remote Data Source

---
+++ frontend/frontend_admin/lib/core/errors/exceptions.dart
@@ -0,0 +1,17 @@
+class ServerException implements Exception {
+  final String message;
+  ServerException({this.message = ''});
+}
+
+class CacheException implements Exception {}
+
+class NetworkException implements Exception {}
+
+class AuthenticationException implements Exception {
+  final String message;
+
+  AuthenticationException({this.message = 'Authentication Failed'});
+
+  @override
+  String toString() => message;
+}
