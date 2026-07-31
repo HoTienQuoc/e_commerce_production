7d71f4ae86b6f684b3b026555360cbe2094dc7ec
HoTienQuoc <hotienquoc0429@gmail.com>
2026-08-01 00:32:29 +0700

    E-commerce Models: Product Variants, Variation & Image

--- backend/backend_admin/ecommerce/models.py
+++ backend/backend_admin/ecommerce/models.py
@@ -85,3 +85,64 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+
+class ProductImage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Product Image')
+    alt_text = models.CharField(max_length=200)
+    order = models.IntegerField(default=0)
+    is_primary = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['order']
+        indexes = [
+            models.Index(fields=['product', 'order'])
+        ]
+
+class ProductVariation(models.Model):
+    """A category of variation like 'Size', 'Color', etc..."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, related_name='variation_types', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    values = JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    class Meta:
+        unique_together = ('Product', 'name')
+
+
+class ProductVariant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, related_name='variation_types', on_delete=models.CASCADE)
+    attributes = JSONField(null=True, blank=True)
+    sku = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    stock = models.IntegerField(default=0)
+    image = models.ForeignKey(ProductImage, null=True, blank=True, on_delete=models.SET_NULL, related_name='variants')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['product']),
+        ]
+
+
+    def save(self, *args, **kwargs):
+        if not self.sku:
+            product_code = self.product.name[:3].upper()
+            variant_code = '-'.join(f"{key[:1]{val[:2]}}" for key, val in sorted(self.attributes.items()))
+            self.sku = f"{product_code}-{variant_code}-{uuid.uuid4().hex[:6].upper()}"
+        super().save(*args, **kwargs)
+
+    @property
+    def effective_price(self):
+        return self.discount_price if self.discount_price else self.price or self.product.price
+
+    
+
+
