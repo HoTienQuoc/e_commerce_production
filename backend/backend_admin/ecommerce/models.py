3ce7e28cbf2a5f9cd77067526f18bb8d4b9a5407
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-31 21:04:41 +0700

    E-commerce Models: Category and Model Meta Class

--- backend/backend_admin/ecommerce/models.py
+++ backend/backend_admin/ecommerce/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import uuid
+from django.conf import settings
+from django.db.models import JSONField
+from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxLengthValidator
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=120, null=True, blank=True)
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        index = [
+            models.Index(fields=['name']),
+            models.Index(fields=['slug'])
+        ]
+        ordering = ['name']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'parent'],
+                name='unique_name_parent_combination'
+            )
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
