e00a503154f873ce979014ae477b5602dcdaa6b5
HoTienQuoc <hotienquoc0429@gmail.com>
2026-07-31 22:26:51 +0700

    Ecommerce Models: The Product Model

--- backend/backend_admin/ecommerce/models.py
+++ backend/backend_admin/ecommerce/models.py
@@ -39,3 +39,49 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0)
+    reviews_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    @property
+    def primary_image(self):
+        """Return the primary image or the first image if no primary is set"""
+        primary = self.images.filter(is_primary=True).first() # pyright: ignore[reportAttributeAccessIssue]
+
+        if not primary:
+            primary = self.images.first() # pyright: ignore[reportAttributeAccessIssue]
+        return primary
+
+    @property
+    def primary_image_url(self):
+        """Return the URL of the primary image"""
+        img = self.primary_image
+        return img.image.url if img and img.image else None
+
+    @property
+    def display_price(self):
+        return self.discount_price if self.discount_price is not None else self.price# pyright: ignore[reportAttributeAccessIssue]
+
+    class Meta:
+        indexes = [
+            models.Index('name', 'category'),
+            models.Index(fields=['price']),
+            models.Index(fields=['rating']),
+            models.Index(fields=['rating']),
+            models.Index(fields=['is_active']),
+        ]
+
+    def __str__(self):
+        return self.name
+    
