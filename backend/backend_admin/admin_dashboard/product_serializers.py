979792fb84a17824cd8bbe6e488b5e036af52517
HoTienQuoc <hotienquoc0429@gmail.com>
2026-08-07 20:49:34 +0700

    Product Serializers: Nested Serializers (Part 2)

--- backend/backend_admin/admin_dashboard/product_serializers.py
+++ backend/backend_admin/admin_dashboard/product_serializers.py
@@ -31,4 +31,39 @@ class ProductImageSerializer(serializers.ModelSerializer):
     def validate(self, data):
         return data
 
+
+class ProductListSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    primary_image_url = serializers.SerializerMethodField()
+    stock_status = serializers.SerializerMethodField()
+    stock = serializers.IntegerField(read_only=True)
+
+    def get_stock_status(self, obj):
+        inventory = getattr(obj, 'inventory', None)
+        return inventory.stock_status if inventory else 'unknown'
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'category', 'category_name', 'price', 'stock', 'discount_price', 'display_price', 'rating', 'primary_image_url', 'stock_status', 'is_active'
+        ]
+
+    def get_primary_image_url(self, obj):
+        request = self.context.get('request')
+        primary = obj.primary_image
+
+        if primary and primary.image:
+            if request:
+                return request.build_absolute_uri(primary.image.url)
+            return primary.image.url
+        return None
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if self.context.get('include_all_images', False):
+            ret['all_image_urls'] = self.get_all_image_urls(instance) # pyright: ignore[reportAttributeAccessIssue]
+        return ret
+
+    
+    
     
