2b1573cb8966e365e6ac9f484a21fda467a06c98
HoTienQuoc <hotienquoc0429@gmail.com>
2026-08-17 19:28:10 +0700

    Integrating Filtering & Caching into the Admin Product ViewSet

--- backend/backend_admin/admin_dashboard/services/products/product_service.py
+++ backend/backend_admin/admin_dashboard/services/products/product_service.py
@@ -7,6 +7,8 @@ from inventory.models import InventoryRecord
 from inventory.services import InventoryService
 from admin_dashboard.product_serializers import (ProductFullSerializer, ProductUpdateSerializer, ProductVariantSerializers)
 from .base_service import BaseService
+from .product_cache_service import ProductCacheService
+from .product_filter_service import ProductFilterService
 
 logger = logging.getLogger(__name__)
 
@@ -17,10 +19,27 @@ class ProductService(BaseService):
         self.request = request
         self.user = request.user if request else None
 
+        # Initialize dependent services
+        self.cache_service = ProductCacheService()
+        self.filter_service = ProductFilterService()
+        self.inventory_service = InventoryService()
+
         # For tracking current product ID in operations
         self.current_product_id = None
 
     def get_filtered_products(self, query_params):
         """Get filtered products using filter service"""
         return self.filter_service.get_filtered_products(query_params)
+
+    def get_filter_options(self):
+        """Get filter options using filter service"""
+        return self.filter_service.get_filter_options()
+
+    def clear_product_cache(self, product_id = None):
+        """Clear product cache using cache service"""
+        self.cache_service.clear_product_cache(product_id)
+
+    
+
+    
     
--- backend/backend_admin/admin_dashboard/views/product_views.py
+++ backend/backend_admin/admin_dashboard/views/product_views.py
@@ -10,7 +10,9 @@ import time
 
 from ecommerce.models import Product
 from inventory.services import InventoryService
-from admin_dashboard.product_serializers import (ProductCreateSerializer, ProductDetailSerializer, ProductFullSerializer, ProductListSerializer, ProductImageSerializer, ProductVariantSerializers)
+from admin_dashboard.product_serializers import (ProductCreateSerializer, ProductUpdateSerializer, ProductDetailSerializer, ProductFullSerializer, ProductListSerializer, ProductImageSerializer, ProductVariantSerializers)
+from admin_dashboard.services.products.product_service import ProductService
+from admin_dashboard.core.cache_util import CacheUtil
 
 class AdminProductViewSet(viewsets.ModelViewSet):
     """ViewSet for managin products in the admin dashboard"""
@@ -21,6 +23,8 @@ class AdminProductViewSet(viewsets.ModelViewSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.inventory_service = InventoryService()
+        self.product_service = ProductService()
+        self.cache_util = CacheUtil(model_name='product')
 
 
     def get_serializer_context(self):
@@ -30,4 +34,32 @@ class AdminProductViewSet(viewsets.ModelViewSet):
         return context
 
     def get_queryset(self):
-        return self.product_service.get_filtered_products(self.request.query_params)
+        return self.product_service.get_filtered_products(self.request.query_params) # pyright: ignore[reportAttributeAccessIssue]
+
+    def get_serializer_class(self):
+        """Return the approriate serializer class based on the action"""
+        if self.action == 'create':
+            return ProductCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return ProductUpdateSerializer
+        elif self.action == 'list':
+            return ProductListSerializer
+        elif self.action == 'retrieve':
+            return ProductFullSerializer
+        return super().get_serializer_class()
+
+    def create(self, request, *args, **kwargs):
+        try:
+            data = request.data.copy()
+
+            if 'stock' in data and not data.get('initial_stock'):
+                data['initial_stock'] = data['stock']
+
+            # Create product with serializer
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+
+            with transaction.atomic():
+                product = serializer.save()
+                
+        return super().create(request, *args, **kwargs)
