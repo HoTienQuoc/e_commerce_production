822119c4c711e30ec5304f099b61db2d06bbe04b
HoTienQuoc <hotienquoc0429@gmail.com>
2026-08-29 21:02:46 +0700

    Presentation Layer: Product List Bloc Events

---
+++ frontend/frontend_admin/lib/features/products/presentation/bloc/product_list/product_list_event.dart
@@ -0,0 +1,108 @@
+part of 'product_list_bloc.dart';
+
+import 'package:frontend_admin/features/products/domain/entities/product_entity.dart';
+
+abstract class ProductListEvent extends Equatable {
+  const ProductListEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+
+class GetPaginatedProductsEvent extends ProductListEvent {
+  final int page;
+  final int pageSize;
+  final String? search;
+  final String? categoryId;
+  final double? minPrice;
+  final double? maxPrice;
+  final String? status;
+
+  final StockStatus? stockStatus;
+  final Map<String, dynamic>? extraParams;
+
+  GetPaginatedProductsEvent({required this.page, required this.pageSize,  this.search,  this.categoryId,  this.minPrice,  this.maxPrice,  this.status,  this.stockStatus,  this.extraParams});
+
+  @override
+  List<Object?> get props => [page, pageSize, search, categoryId, status, stockStatus, extraParams];
+}
+
+class SearchProductsEvent extends ProductListEvent {
+  final String? search;
+  final bool resetPage;
+  final bool resetFilters;
+
+  SearchProductsEvent({ this.search,  this.resetPage = false,  this.resetFilters = false});
+
+  @override
+  List<Object?> get props => [search, resetPage, resetFilters];
+}
+
+class SetLoadingEvent extends ProductListEvent {
+  final bool isLoading;
+
+  const SetLoadingEvent(this.isLoading);
+
+  @override
+  List<Object> get props => [isLoading];
+}
+
+class SetCachedProductsEvent extends ProductListEvent {
+  final List<ProductEntity> products;
+  final int page;
+  final int totalCount;
+
+  SetCachedProductsEvent({required this.products, required this.page, required this.totalCount});
+}
+
+class ClearFiltersEvent extends ProductListEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadMoreProductsEvent extends ProductListEvent {
+  final int page;
+  final int pageSize;
+
+  LoadMoreProductsEvent({required this.page, required this.pageSize});
+
+  @override
+  List<Object> get props => [page, pageSize];
+}
+
+class BulkDeleteProductsEvent extends ProductListEvent {
+  final List<String> productIds;
+
+  const BulkDeleteProductsEvent({required this.productIds});
+
+  @override
+  List<Object?> get props => [productIds];
+}
+
+class GetProductCategoriesEvent extends ProductListEvent {}
+
+class GetProductFiltersEvent extends ProductListEvent {}
+
+class ToggleProductStatusEvent extends ProductListEvent {
+  final String id;
+
+  const ToggleProductStatusEvent({required this.id});
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class ClearProductErrorEvent extends ProductListEvent {}
+
+class ResetProductListEvent extends ProductListEvent {}
+
+class ProductDeletedEvent extends ProductListEvent {
+  final String productId;
+
+  const ProductDeletedEvent({required this.productId});
+
+  @override
+  List<Object?> get props => [productId];
+}
+
