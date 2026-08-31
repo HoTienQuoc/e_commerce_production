5b1bbafb2a7a39e2edff2cda5efb688a663931d9
HoTienQuoc <hotienquoc0429@gmail.com>
2026-08-31 22:56:52 +0700

    Product Create Form UI (Part 1)

---
+++ frontend/frontend_admin/lib/features/products/presentation/pages/product_forms/product_create_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend_admin/features/products/presentation/bloc/product_details/product_details_bloc.dart';
+
+class ProductCreatePage extends StatelessWidget {
+  const ProductCreatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Product'),
+        backgroundColor: Theme.of(context).cardColor,
+        elevation: 1,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: ProductCreateForm(
+              onSave: (product, images) async {
+                try {
+                  context.read<ProductDetailsBloc>().add(
+                    CreateProductEvent(product: product, images: images),
+                  );
+                  return product;
+                } catch (e) {
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Error: ${e.toString()}'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                  return null;
+                }
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
