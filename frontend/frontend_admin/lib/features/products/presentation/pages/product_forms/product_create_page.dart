05c15f6446253a8e74e50b9c8a7f1855597f576e
HoTienQuoc <hotienquoc0429@gmail.com>
2026-09-01 15:32:12 +0700

    Product Create Form UI (Part 2)

--- frontend/frontend_admin/lib/features/products/presentation/pages/product_forms/product_create_page.dart
+++ frontend/frontend_admin/lib/features/products/presentation/pages/product_forms/product_create_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend_admin/features/products/presentation/bloc/product_details/product_details_bloc.dart';
+import 'package:frontend_admin/features/products/presentation/widget/product_form_widgets/product_create_form.dart';
 
 class ProductCreatePage extends StatelessWidget {
   const ProductCreatePage({super.key});
