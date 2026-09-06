90e4592e26be560f89a90fe9b2c6287b91904076
HoTienQuoc <hotienquoc0429@gmail.com>
2026-09-06 20:09:35 +0700

    Presentation Layer: Variant BLoC Events (Part 1+2)

---
+++ frontend/frontend_admin/lib/features/variations/presentation/bloc/product_variant_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:equatable/equatable.dart';
+import 'package:frontend_admin/features/variations/domain/entities/product_variant_entity.dart';
+import 'package:frontend_admin/features/variations/domain/entities/product_variations_entity.dart';
+
+part 'product_variant_event.dart';
+part 'product_variant_state.dart';
--- frontend/frontend_admin/lib/features/variations/presentation/bloc/product_variant_state.dart
+++ frontend/frontend_admin/lib/features/variations/presentation/bloc/product_variant_state.dart
@@ -1,2 +1,3 @@
 // Variation definitions - the abstract options you offer (e.g., Color, Size)
 // Product Variants - The concrete, sellable combinations that result from those definitions (e.g., Color:Red, Size: Medium)
+part of 'product_variant_bloc.dart';
